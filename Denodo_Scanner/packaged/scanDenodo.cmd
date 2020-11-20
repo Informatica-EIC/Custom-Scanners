e3c2fa03afa3740709ac03d4a85f100af18e22ce
darren_wrigley <dwrigley@informatica.com>
2020-11-20 11:44:24 -0500

    removing un-necessary files

--- Denodo_Scanner/packaged/scanDenodo.cmd
+++
@@ -1,7 +0,0 @@
-
-rem - this script assumes that JAVA_HOME is set, and java is in the system path "java -version"  command should return 1.8 or newer
-
-echo starting...  java -cp "denodoCustomScanner.jar;lib/*" com.infa.edc.scanner.jdbc.DenodoScanner %1 %2
-call java -cp "denodoCustomScanner.jar;lib/*" com.infa.edc.scanner.jdbc.DenodoScanner %1 %2
-
-echo 'Finished'
